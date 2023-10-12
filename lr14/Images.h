d76daeead881b8e9000602f215e9f1f10aa1261f
Егор Пыжов <pizhovegor@gmail.com>
2023-10-12 22:54:18 +0400

    Добавьте файлы проекта.

---
+++ lr14/Images.h
@@ -0,0 +1,8 @@
+
+void Image0(HDC hdc, int cx, int cy, COLORREF color);
+void Image1(HDC hdc, int cx, int cy, COLORREF color);
+void Image2(HDC hdc, int cx, int cy, COLORREF color);
+void Image3(HDC hdc, int cx, int cy, COLORREF color);
+void Image4(HDC hdc, int cx, int cy, COLORREF color);
+
+
